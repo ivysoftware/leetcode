525c1951b1c9d60a89217179d446f60e490dec97
ivysoftware <ivysoftwarenankai@gmail.com>
2013-08-04 20:52:05 +0800

    Update MinimumCutOfPalindrom.cpp

--- MinimumCutOfPalindrom.cpp
+++ MinimumCutOfPalindrom.cpp
@@ -1,3 +1,4 @@
+
 /*Minimum cut of palindrom*/
 #include <iostream>
 #include <vector>
@@ -9,6 +10,7 @@ int MinimumCutOfPalindrom(char* src){
 	unsigned int length = 0;
 	while(*(src+length) != '\0')
 		length ++;
+    cout<<length<<endl;
 	int IsCut[length];
 	//memset(IsCut, -1, length);  //memset is not set
 	for(int i=0; i< length; i++)
@@ -16,7 +18,7 @@ int MinimumCutOfPalindrom(char* src){
 	IsCut[0] = 1; 
 
 	int pos = 1;
-	while(pos < length)
+	while(pos < length){
 		//odd symmetric
 		int i;
 		for(i=1; i <= pos && (pos+i) < length; i++){
@@ -26,34 +28,41 @@ int MinimumCutOfPalindrom(char* src){
 			IsCut[pos+i] = 0;
 			
 		}
-		if(i > 1){
+		if(i > 1 && i <= pos){
+            IsCut[pos-i] = 1;
+            IsCut[pos] = 0;
+            pos = pos + i;
+        }else if (i > 1){
+            IsCut[pos] = 0;
 			pos = pos+i;
-			IsCut[pos] = 0;
-		}
+		    continue;	
+        } 
+          
+
 		//even symmetric		
-		int left = pos-1;
+		int left = pos- 1 ;
 		int right = pos;
-		while(left >= 0 && right < length && src[left] == src[right]){
+		for(; left >= 0 && right < length && src[left] == src[right]; left--, right++){
 			IsCut[left] = 0;
 			IsCut[right] = 0;
-			left --;
-			right ++;
 		}
-		if(left > = 0)
+		if(left >= 0)
 			IsCut[left] = 1;
-		if(right < length)
-			IsCut[right] = 1;
-		pos = (pos == right) ? pos+1 : right+1;		
+		if(right < length && pos < right)
+			IsCut[right - 1] = 1;
+        else if(pos == right)
+            IsCut[pos] = 1;
+		pos = (pos == right) ? pos+1 : right;		
 	}
 	int MinCut = 0;
 	for(int i = 0; i< length -1; i++){
-		if(IsCut == 1)
+		if(IsCut[i] == 1)
 			MinCut ++;
-		if(IsCut == -1)
-			cout<<i<<" "
+		//if(IsCut[i] == -1)
+		cout<<i<<" "<<IsCut[i]<<" ";
 	}
 	cout<<endl;
-	return Mincut;
+	return MinCut;
 } 
 int main(){
 	char src[1256];
